8756c3c61aba422e4f1c9dfe789d9462d54950c0
afeiship <1290657123@qq.com>
2017-08-11 22:10:51 +0800

    issue: intial

---
+++ src/next-clone.js
@@ -0,0 +1,24 @@
+(function () {
+
+  var global = global || this || self || window;
+  var nx = global.nx || require('next-js-core2');
+
+  nx.clone = function (target, source, deep) {
+    var isPlainObject = nx.isPlainObject,
+      isArray = nx.isArray;
+    for (var key in source)
+      if (deep && (isPlainObject(source[key]) || isArray(source[key]))) {
+        if (isPlainObject(source[key]) && !isPlainObject(target[key]))
+          target[key] = {}
+        if (isArray(source[key]) && !isArray(target[key]))
+          target[key] = []
+        nx.clone(target[key], source[key], deep)
+      } else if (source[key] !== undefined) target[key] = source[key]
+    return target;
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.clone;
+  }
+
+}());
